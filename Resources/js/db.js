be9c571602273eda0d82341f2a2822ef10746584
Brett <bjones027@gmail.com>
2013-03-20 14:47:34 -0500

    fixed getScalar from infinite loop

--- Resources/js/db.js
+++ Resources/js/db.js
@@ -1,48 +1,42 @@
 var Database = function (filepath) {
   var self = this;
+  window.DB = Ti.Database.openFile(Ti.Filesystem.getFile(Ti.Filesystem.getApplicationDataDirectory(), 'application.db'));
 
-  self.run = function () {
-    window.DB = Ti.Database.openFile(Ti.Filesystem.getFile(Ti.Filesystem.getApplicationDataDirectory(), 'application.db'));
-
-    //Create a table and insert values into it
-    window.DB.execute("CREATE TABLE IF NOT EXISTS Users(id INTEGER, firstName TEXT)");
-    window.DB.execute("INSERT INTO Users VALUES(1,'Joe Bloggs')");
-    console.log(self.executeGetRows("SELECT * FROM Users", Person));
-
-    //Select from Table
-  };
-
-  self.executeGetRows = function (sql, expectedReturnType) {
+  self.executeGetRows = function (sql) {
     var rows = window.DB.execute(sql);
     var result = [];
 
     while(rows.isValidRow()) {
-      eval(String.format("var item = new {0}();", expectedReturnType));
+      var item = {};
       var i = 0;
 
       while (true) {
-        try {
-          var val = rows.field(i);
-          var key = rows.fieldName(i);
-          var fmtStr = (typeof val == 'string' || val instanceof String) ? "item.{0} = '{1}'" : "item.{0} = {1}"
-          var setter = String.format(fmtStr, key, val);
-          console.log('Running ' + setter);
-          eval(setter);
-          i++;
-        } catch (e) { break; }
+        var key, val;
 
+        try {
+          val = rows.field(i);
+          key = rows.fieldName(i);
+        } catch (e) { break; }
+        item[key] = val;
+        i++;
       };
 
       result.push(item); 
       rows.next();
 
     };
+    rows.close();
     return result; 
   };
-};
 
-var Person = function () {
-  var self = this;
-  self.firstName = "";
-  self.id = 0;
+  self.executeGetScalar = function (sql) {
+    var row = window.DB.execute(sql);
+    var result = null;
+    if (row.isValidRow()) {
+      result = row.field(0);
+    };
+    row.close();
+    return result;
+  };
+
 };
