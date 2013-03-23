f79daa0349e6ca999fd58d0261b44cf49f49faee
Brett <bjones027@gmail.com>
2013-03-23 00:09:32 -0500

    fixed styles for main view

--- Resources/js/app.js
+++ Resources/js/app.js
@@ -18,9 +18,9 @@ var Util = {
 	makeSvg: function (svgFile, fallback) {
 		if (!fallback) { fallback = 'img/climacons/fallback.jpg'; }
 		// return String.format('<object data="{0}" type="image/svg+xml"><img src="{1}" /></object>', svgFile, fallback);
-		return String.format('<img src="{0}"/>', svgFile);
+		// return String.format('<img src="{0}" $paramFill="green"/>', svgFile);
+		return String.format("url({0})", svgFile);
 	},
-
 	toForecast: function (raw) {
 		return _.map(raw, function (item) {
 			return new Forecast(item);
